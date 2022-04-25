c6d5d5b28d19e4d0f4a733c7406de1fc7cccfe32
Sini80 <98733025+Sini80@users.noreply.github.com>
2022-04-25 10:50:09 +0300

    lisätty demo8
    ulos kirjautuminen nappi lisätty ja poistuu näkyviltä kun olet kirjautunut sisään.

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", onkokirjautunut);
 
-function onkokirjautunut(){
- let kirjautunut = 'kylla';
- if(kirjautunut === 'kylla'){
-     document.getElementById('tervetulo_teksti',).textContent += localStorage.getItem("nimi");
+function onkokirjautunut() {
+    
+ if (logalStorage.getItem("kirjautunut") === "kylla") {
+     document.getElementById("tervetulo_teksti").textContent += localStorage.getItem("nimi");
+     document.getElementById("kirjautumis_lomake").style.display = "none";
+    
  }
 }
 
-function kirjaudu(){
+function kirjaudu() {
     localStorage.setItem("nimi", document.getElementById("nimi").value);
+    logalStorage.setItem("kirjautunut", "kylla");
 }
-}
+
+function 
