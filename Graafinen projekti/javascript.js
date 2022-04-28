e3afc9193a8c57e7f6ea5a7fb8d129351a610756
Sini80 <98733025+Sini80@users.noreply.github.com>
2022-04-28 20:25:24 +0300

    Lisätty Graafinen projekti, index.html, style.css, javascript.js
    Html, css, javascript, keskeneräinen vielä

---
+++ Graafinen projekti/javascript.js
@@ -0,0 +1,18 @@
+const insert = document.addEventListener('keydown', (event) => {
+console.log(event);
+
+insert.innerHTML = `
+<div class="key">
+${event.key === ' '? 'Space' : event.key }
+<small>event.key</small>
+</div>
+<div class="key">
+${event.KeyCode}
+<small>event.keyCode</small>
+</div>
+<div class="key">
+${event.code}
+<small>event.code</small>
+</div>
+`;
+})
